ece249b118e4a6985029791703f8391afe4d5d3a
vinutha <vinutha418@gmail.com>
2023-02-08 14:15:34 -0500

    added dockerconfigurations

---
+++ code/docker/springbootDocker/src/main/java/com/Vinutha/springbootDocker/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.Vinutha.springbootDocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String helloDocker(){
+        return "Hello Docker user";
+    }
+}
